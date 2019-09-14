02972e0515bb3f164f1fb71ab7fe12536835b8c2
AislynnEdmiston <davidandaislynn@gmail.com>
2019-09-14 14:04:41 -0400

    LocationsList complete, needs styled though

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,7 +1,9 @@
 import React from "react";
 
-export default function LocationCard({ name, type, dimension, residents }, props) {
+export default function LocationCard(props) {
   return <div>
-    <h3>{props.name}</h3>
+    <h3>{props.place.name}</h3>
+    <p>Dimension: {props.place.dimension}</p>
+    <p>Type: {props.place.type}</p>
   </div>;
 }
